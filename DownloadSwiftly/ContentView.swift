26c7caa829caf7c4e68236459b8191d0759602fa
Rajdeep Bharati <rajdeep@macports.org>
2020-05-03 18:58:51 +0530

    Initial Commit

---
+++ DownloadSwiftly/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DownloadSwiftly
+//
+//  Created by Rajdeep Bharati on 03/05/20.
+//  Copyright © 2020 rb. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
